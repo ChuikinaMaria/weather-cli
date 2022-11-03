44ae5920ef30f3dfcaa81347a9d005ee40fe0092
Maria Chuikina <4uikina@gmail.com>
2022-11-03 16:21:27 +0100

    ''

--- weather.js
+++ weather.js
@@ -1,9 +1,19 @@
 #!/usr/bin/env node
 import { getArgs } from './helpers/args.js';
 import { printError, printSuccess, printHelp } from './services/log.service.js';
-import { saveKeyValue } from './services/storage.service.js';
+import { saveKeyValue, getKeyValue } from './services/storage.service.js';
 
-const initCLI = () => {
+const saveToken = async (token) => {
+    try{
+        await saveKeyValue('token', token);
+        printSuccess('Token is saved')
+    } catch (e) {
+        printError(e.message);
+    }
+
+}
+
+const initCLI = async () => {
     const args = getArgs(process.argv);
     
     if (args.h) {
@@ -14,7 +24,7 @@ const initCLI = () => {
         
     }
     if (args.t) {
-        saveKeyValue('token', args.t);
+        return saveToken(args.t);
         //save token
     }
     // вывести погоду
